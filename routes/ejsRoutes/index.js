f246be256a39637e71b594a8fe7b93ac11465113
Abhijith ps <abhijithabi22102001@gmail.com>
2023-11-21 11:57:45 +0530

    update animation

--- routes/ejsRoutes/index.js
+++ routes/ejsRoutes/index.js
@@ -3,17 +3,20 @@ var router = express.Router();
 const about = require("../../models/aboutUs");
 const destination = require("../../models/destination");
 const package = require("../../models/package");
+const faq = require("../../models/faq");
 /* GET home page. */
 
 router.get("/", async function (req, res, next) {
   const aboutData = await about.findOne();
   const destinationData = await destination.findOne();
   const packageData = await package.findOne();
+  const faqData = await faq.find();
   res.render("index", {
     title: "Express",
     aboutData,
     destinationData,
     packageData,
+    faqData,
   });
 });
 
