b328e7cd41eb0e59af4fc30f4da5880878961ddb
Abhijith ps <abhijithabi22102001@gmail.com>
2023-11-30 11:21:38 +0530

    updates on some ui

--- routes/ejsRoutes/sandtours.js
+++ routes/ejsRoutes/sandtours.js
@@ -3,14 +3,17 @@ var router = express.Router();
 const about = require("../../models/aboutUs");
 const package = require("../../models/package");
 const faq = require("../../models/faq");
+const activity = require("../../models/activity");
 /* GET home page. */
 router.get("/", async function (req, res, next) {
   const packageData = await package.find();
   const aboutData = await about.findOne();
   const faqData = await faq.find();
+  const activityData = await activity.find();
   res.render("sandtours", {
     title: "Express",
     packageData,
+    activityData,
     faqData,
     aboutData,
   });
